726a089b3b7c8a6624b24a019f19893e5f5e05ae
Felipe Ferreira <felipesqf@gmail.com>
2021-07-14 15:13:09 +1000

    removing cluster

--- ecs-cluster.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecs_cluster" "ecs_cluster" {
-  name               = "my-ecs-fargate"
-  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
-
-  tags = {
-    Environment = "prod"
-  }
-}
